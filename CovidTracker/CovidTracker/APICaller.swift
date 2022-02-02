b3ae9130c76abe6ec2350ae70dae765569cb8afe
onur.basdas <onur.basdas@3pounds.io>
2022-02-02 14:06:52 +0300

    FilterVC design and get service completed.

---
+++ CovidTracker/CovidTracker/APICaller.swift
@@ -0,0 +1,60 @@
+//
+//  APICaller.swift
+//  CovidTracker
+//
+//  Created by Onur Başdaş on 2.02.2022.
+//
+
+import Foundation
+
+class APICaller {
+    static let shared = APICaller()
+    
+    private init() {}
+    
+    private struct Constants {
+        static let allStatesUrl = URL(string: "https://api.covidtracking.com/v2/states.json")
+    }
+    
+    enum DataScope {
+        case national
+        case state(State)
+    }
+    
+    public func getCovidData(
+        for scope: DataScope,
+        completion: @escaping(Result<String, Error>) -> Void
+    ) {}
+    
+    public func getStateList(
+        completion: @escaping (Result<[State], Error>) -> Void
+    ) {
+        guard let url = Constants.allStatesUrl else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let result = try JSONDecoder().decode(StateListResponse.self, from: data)
+                let states = result.data
+                completion(.success(states))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+
+}
+
+// MARK: Models
+
+struct StateListResponse: Codable {
+    let data: [State]
+}
+
+struct State: Codable {
+    let name: String
+    let state_code: String
+}
--- CovidTracker/CovidTracker/ViewController.swift
+++ CovidTracker/CovidTracker/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var scope: APICaller.DataScope = .national
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Covid Cases"
+        createFilterButton()
+    }
+    
+    private func createFilterButton() {
+        let buttonTitle: String = {
+            switch scope {
+            case .national: return "National"
+            case .state(let state): return state.name
+            }
+        }()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: buttonTitle,
+            style: .done,
+            target: self,
+            action: #selector(didTapFilter))
+    }
+    
+    @objc private func didTapFilter() {
+        let vc = FilterVC()
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
     }
 
 
