d41bd36df4e8e731a14dbce5e965e3ff8df05cc7
onur.basdas <onur.basdas@3pounds.io>
2022-02-02 12:53:23 +0300

    First commit, good commit :)

---
+++ CovidTracker/CovidTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidTracker
+//
+//  Created by Onur Başdaş on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
